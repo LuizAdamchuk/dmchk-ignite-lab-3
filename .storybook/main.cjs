dc5460b1c3527db7b861ed4a0c1c7c9f13e2a157
LuizAdamchuk <adamchuk.junior@gmail.com>
2022-10-13 12:20:28 -0300

    fix: Rendering components on storybook MIME error

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,28 @@
+const { config } = require("@storybook/addon-actions");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/dmchk-ignite-lab-3/";
+    }
+
+    return config;
+  },
+};
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,27 @@
 import { clsx } from "clsx";
 import { Slot } from "@radix-ui/react-slot";
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
   const Comp = asChild ? Slot : "span";
   return (
     <Comp
       className={clsx(
-        "py-4 px-3 w-full bg-cyan-500 rounded font-semibold text-black text-sm transition-colors hover:bg-cyan-300 focus:right-2 ring-white"
+        "py-3 px-4 w-full bg-cyan-500 rounded flex items-center  justify-center font-semibold text-black text-sm transition-colors hover:bg-cyan-300 focus:right-2 ring-white",
+        className
       )}
+      {...props}
     >
       {children}
     </Comp>
